9674ae94ba55bb8f6b9aa2f0d458a1840612429c
Tyler McCarthy <96397434+TylerMcCarthyGitHub@users.noreply.github.com>
2023-11-27 13:48:38 +0000

    Update layout_parse_detectron.py

--- layout_parse_detectron.py
+++ layout_parse_detectron.py
@@ -1,57 +1,176 @@
-// get response from genext - activates scraper
-app.post('/genext', async (req, res) => {
-	res.status(503).json({message: "Genext endpoint closed for maintenance"})	// closed as spawning too many chrome windows
-	return;
-	let response = "";
-	console.log(req.body.prompt);
-	let driver = await new Builder().forBrowser(Browser.CHROME).build();
-	try {
-		await driver.get('https://genext.bmwgroup.net/#/chat');
-		await driver.sleep(2000)
-		await driver.findElement(By.id('idToken2')).sendKeys('Q510579');
-		await driver.findElement(By.id('idToken3')).sendKeys('Thisisanewpasswordpleaselogin:',Key.ENTER)
-		await driver.sleep(500)
-		await driver.findElement(By.xpath('/html/body/app-root/genext-sidebar/div/div[2]/div/div/app-landing/div/div[3]/div/ds-box/div/div[3]/ds-box-content/div[2]/a')).click();
-		
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[1]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[2]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[3]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[4]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[5]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[6]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[7]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('//*[@id="disclaimer-en"]/div[2]/ds-form-field/div/ds-checkbox-group[8]/div')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('/html/body/div/div[2]/div/div/ds-box/div/div[4]/ds-box-footer/ds-button')).click();
-		await driver.sleep(50);
-		await driver.findElement(By.xpath('/html/body/app-root/genext-sidebar/div/div[2]/div/div/app-landing/div/div[3]/div/ds-box/div/div[3]/ds-box-content[2]/div[2]/button')).click();
-		await driver.sleep(500);
-		await driver.findElement(By.xpath('/html/body/app-root/genext-sidebar/div/div[2]/div/div/app-chat/div/ds-box/div/div[3]/ds-box-content/div[2]/div/div[2]/genext-chat-parameters/form/ds-form-field[3]/div/div/input')).sendKeys('0');
-		let prompt = "here is my question: " + JSON.stringify(req.body.question) + " from this data (exclude data from LLP2-1): " + JSON.stringify(req.body.data) + " your response must start with X_X";
-		await driver.findElement(By.xpath('/html/body/app-root/genext-sidebar/div/div[2]/div/div/app-chat/div/ds-box/div/div[3]/ds-box-content/div[2]/div/div[1]/div[2]/genext-chat-prompt/form/ds-form-field/div/div/textarea')).sendKeys(prompt);
-		await driver.findElement(By.xpath('/html/body/app-root/genext-sidebar/div/div[2]/div/div/app-chat/div/ds-box/div/div[3]/ds-box-content/div[2]/div/div[1]/div[2]/div/div[2]/button')).click();
-		await driver.sleep(10000)
-		
-		let html = await driver.getPageSource()
+const UIService = {
 
-		let html1 = html.split("<p>X_X");
-		let html2 = html1[1].split("</p>");
-		let response = html2[0];
-		await driver.quit();
+    startupUI: function() {
+        UIService.parentStationClickHandler();
+        UIService.showChildStation(false);
+        UIService.toggleStationImageContainer(false);
+        $("#mission_barchart").hide();
+        $("#mission_overview").hide();
+        UIService.updateCellDropdown();
+        DataService.fetchRoutesData().then(() => {
+            DataService.fetchLatestCellData().then(cellData => {
+                const latestImageUrls = DataService.extractImageUrlsFromMissionData(cellData);
+                ImageService.updateImageContainer('image_container', latestImageUrls);
+            }).catch(error => {
+                console.error("Failed to fetch latest cell data:", error);
+            });
+        }).catch(error => {
+            console.error("Failed to fetch routes data:", error);
+        });
+    },
 
-		res.status(200).json({"response":response});
+    parentStationClickHandler: function() {
+        // Initialise the parent station click handler
+        $(".parent_station").click(function() {
+            UIService.toggleStationLevel($(this).attr("id"));
 
-	} finally {
-		
+        })
+    },
 
-		
-	}
+    showChildStation(show) {
+        if(show == true) {
+            $(".child_station").show();
+            let modelViewer = document.querySelector("#model_viewer");
+            modelViewer.cameraTarget = $("#" + stationName).data("position");
+            modelViewer.cameraOrbit = $("#" + stationName).data("orbit");
+        } else {
+            $(".child_station").hide();
+        }
+    },
 
-});
+    updateCellDropdown: function() {
+        DataService.fetchCells();
+    },
+    
+    updateCellOverview: function() {
+        $("#date_created").val(DataService.cellDateCreated);
+        $("#last_run").val(DataService.cellLastRun);
+        $("#duration").val(DataService.cellDuration);
+        $("#inspections_num").val(DataService.cellNumInspections);
+    },
+
+    toggleStationImageContainer: function(show) {
+        const stationImageContainer = document.getElementById("R698784236320332819");
+        if (stationImageContainer) {
+            stationImageContainer.style.display = show ? 'block' : 'none';
+        } else {
+            console.error("StationImageContainer not found");
+        }
+    },
+
+    bindStationHotspots: function() {
+        $(".station_hotspot").click(function () {
+            UIService.selectStation($(this).attr("id"));
+        });
+    },
+
+    toggleLatestImageContainer: function(show) {
+        const stationImageContainer = document.getElementById("R698783736046332814");
+        if (stationImageContainer) {
+            stationImageContainer.style.display = show ? 'block' : 'none';
+        } else {
+            console.error("StationImageContainer not found");
+        }
+    },
+
+    selectStation: async function(stationId) {
+        console.log("Station selected:", stationId);
+    
+        try {
+            const selectedStationData = await DataService.fetchStationMissionData(stationId);
+    
+            let allImageData = [];
+    
+            for (let descriptor of selectedStationData.machine_descriptors) {
+                for (let missionFile of descriptor.missions) {
+                    const missionData = await DataService.fetchMissionJson(missionFile);
+    
+                    const imageData = DataService.processMissionData(missionData)
+                        .filter(item => item.url.includes(descriptor.fullStationName));
+    
+                    allImageData.push(...imageData);
+                }
+            }
+            ImageService.updateImageContainer('station_image_container', allImageData);
+            UIService.toggleStationImageContainer(true);
+        } catch (error) {
+            console.error("Error fetching data for station:", error);
+        }
+    },
+
+    toggleStationLevel: function (
+        parent_id) {
+        if($("#"+parent_id).hasClass("currentlyOpen")) {
+            console.log("parent station", parent_id)
+            console.log(parent_id);
+            $('#model_viewer').children('button').each(function () {
+                if($(this).hasClass("station_hotspot") || $(this).hasClass("station_hotspot_hoverable")) {
+                    if($(this).hasClass("child_station")) {
+                        $(this).hide();
+                    } else {
+                        $(this).show();
+                    }
+                }
+            });
+    
+            document.getElementById(parent_id).removeChild(document.getElementById(parent_id).lastChild);
+            /*$("#"+parent_id).attr("data-childstations").split(",").forEach(station => {
+                $("#"+station.replace(/ /g,'')).hide()
+            }); */
+            $("#"+parent_id).removeClass("currentlyOpen");
+        } else {
+            if($("#"+parent_id).attr("data-childstations").length > 1) {
+                $('#model_viewer').children('button').each(function () {
+                    if($(this).hasClass("station_hotspot") || $(this).hasClass("station_hotspot_hoverable")) {
+                        if($(this).attr("id") != parent_id) {
+                            // $(this).hide();
+                        }
+                    }
+                });
+                $("#"+parent_id).attr("data-childstations").split(",").forEach(station => {
+                    $("#"+station.replace(/ /g,'')).show()
+                });
+    
+                $("#"+parent_id).append('<span id="tempParentIcon" class="fa fa-sort-desc"></span>');
+                $("#"+parent_id).addClass("currentlyOpen");
+            }
+        }
+    },
+
+    emptyModelViewerContainer: function(){
+        $("#model_viewer_container").empty();
+        console.log("modelviewercontainer emptied");
+    },
+
+    updateAreaDropdown: function(value) {
+        $("#P18_AREA_DROPDOWN").val(value); // Change the model dropdown to show the current selected production line
+    },
+
+    showOverlay: function(show) {
+        const overlay = document.getElementById("overlay_containerDEFAULT");
+        if (overlay) { // Check if the overlay element exists
+            if (show) {
+                overlay.style.display = 'block';
+            } else {
+                overlay.style.display = 'none';
+            }
+        } else {
+            console.error("Overlay container not found");
+        }
+    },    
+
+    showBarChart: function(show) {
+        const barChart = document.getElementById('mission_barchart');
+        if (barChart) {
+            barChart.style.display = show ? 'block' : 'none';
+        } else {
+            console.error("Bar chart container not found");
+        }
+    },    
+
+    updateModelViewer: function(modelHTML) {
+        const viewerContainer = document.getElementById('model_viewer_container');
+        viewerContainer.innerHTML = modelHTML;
+    },
+
+};
